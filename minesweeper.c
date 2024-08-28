b046814305d467c1de295741d338573cb179b050
squee72564 <rodriguez.al183@gmail.com>
2024-08-28 15:14:32 -0700

    Removed deprecated function

--- minesweeper.c
+++ minesweeper.c
@@ -31,9 +31,9 @@ static MineSweeperApp* app_alloc() {
     // Alloc Scene Manager and set handlers for on_enter, on_event, on_exit 
     app->scene_manager = scene_manager_alloc(&minesweeper_scene_handlers, app);
     
-    // Alloc View Dispatcher and enable queue
+    // Alloc View Dispatcher
     app->view_dispatcher = view_dispatcher_alloc();
-    view_dispatcher_enable_queue(app->view_dispatcher);
+    //
     // Set View Dispatcher event callback context and callbacks
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
     view_dispatcher_set_custom_event_callback(app->view_dispatcher, minesweeper_custom_event_callback);
