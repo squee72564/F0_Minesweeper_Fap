5fdcbe297b46034057597d92f8ef8ab376a2482b
Alex Rodriguez <rodriguez.al183@gmail.com>
2026-02-14 10:56:05 -0800

    chore(config): Centralize shared defines into config header

---
+++ helpers/mine_sweeper_config.h
@@ -0,0 +1,22 @@
+#ifndef MINE_SWEEPER_CONFIG_H
+#define MINE_SWEEPER_CONFIG_H
+
+// Application tag for logging
+#define TAG "Mine Sweeper Application"
+
+// Storage Helper Defines
+#define MINESWEEPER_SETTINGS_FILE_VERSION 3
+#define MINESWEEPER_SETTINGS_FILE_VERSION_MIN_SUPPORTED 2
+#define CONFIG_FILE_DIRECTORY_PATH EXT_PATH("apps_data/mine_sweeper_redux")
+#define MINESWEEPER_SETTINGS_SAVE_PATH CONFIG_FILE_DIRECTORY_PATH "/mine_sweeper_redux.conf"
+#define MINESWEEPER_SETTINGS_SAVE_PATH_TMP MINESWEEPER_SETTINGS_SAVE_PATH ".tmp"
+#define MINESWEEPER_SETTINGS_HEADER "Mine Sweeper Redux Config File"
+
+#define MINESWEEPER_SETTINGS_KEY_WIDTH "BoardWidth"
+#define MINESWEEPER_SETTINGS_KEY_HEIGHT "BoardHeight"
+#define MINESWEEPER_SETTINGS_KEY_DIFFICULTY "BoardDifficulty"
+#define MINESWEEPER_SETTINGS_KEY_FEEDBACK "FeedbackEnabled"
+#define MINESWEEPER_SETTINGS_KEY_WRAP "WrapEnabled"
+#define MINESWEEPER_SETTINGS_KEY_SOLVABLE "EnsureSolvable"
+
+#endif // MINE_SWEEPER_CONFIG_H
